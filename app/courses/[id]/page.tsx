049feacf755f05c8d04bb4c641b7c0284940c198
Rashnotech <rashnotech@gmail.com>
2026-03-09 21:43:17 +0100

    Fix: update the courses param to work properly

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -2,6 +2,6 @@ import PageClient from "./page-client"
 
 export const dynamic = "force-dynamic"
 
-export default function Page() {
-  return <PageClient />
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
+  return <PageClient params={params} />
 }
